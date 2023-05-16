2b1d17e9aad6be745ade0acaccaa4277f2d690be
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-16 19:51:24 +0100

    created oper.py

---
+++ importing/utils/oper.py
@@ -0,0 +1,7 @@
+def read_file(file):
+    with open(file,"r") as file_var:
+        return file_var.read().split("\n")
+
+def save_content(file,content):
+    with open(file,"w") as file_var:
+        file_var.write(content)
