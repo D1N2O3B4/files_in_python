6b6ffd08cd37975da9316aa8599d91195a4500f1
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-18 12:51:29 +0100

    edited execute.py

--- importing/execute.py
+++ importing/execute.py
@@ -1,4 +1,5 @@
-from utils.oper import read_file,save_content
+from utils.store.oper import save_content,read_file
+from utils.pear import peep
 
 edit = save_content("importing/data/data.txt","We were on a bridge near West gate \nbut we saw you jumping into the lake \nhow did you manage the fall bro")
 
