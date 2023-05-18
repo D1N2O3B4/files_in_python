aa53620ee39f90c31ecd046b70e4c72b3965ae7b
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-19 00:32:09 +0100

    modified pear.py to return function in pear.py

--- importing/execute.py
+++ importing/execute.py
@@ -1,4 +1,4 @@
-from utils.store.oper import save_content,read_file
+from utils.pear import peep
 
 print(__name__)
 # edit = save_content("importing/data/data.txt","We were on a bridge near West gate \nbut we saw you jumping into the lake \nhow did you manage the fall bro")
--- importing/utils/pear.py
+++ importing/utils/pear.py
@@ -11,3 +11,5 @@ class NotFound(Exception):
     pass
 
 print(__name__)
+if __name__ == "__main__":
+    print(peep(["Sandy","Spongebob","Patrick"],lambda x: x, "Patrick"))
