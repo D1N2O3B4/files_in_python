c8f6ef09cf005b4b4c9279a3e8bbf2a56af278d2
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-21 12:28:46 +0100

    created app.py

---
+++ Milestone_2/app.py
@@ -0,0 +1,36 @@
+from utils.database import add_book,remove_book,read_book,list_all
+
+USER_CHOICE = """Enter:
+-a to add books to the database
+-d to remove books from the database
+-r to mark book as read
+-l to list all books in the database
+-f in repairs
+-q to quit app
+"""
+# operations = {"a":add_book,
+#               "d":remove_book,
+#               "r":read_book,
+#               "l":list_all}
+
+def choice():
+    user_input = input(USER_CHOICE)
+    while user_input != "q":
+        if user_input == "a":
+            title = input("Input book title: ")
+            author = input("Input book author: ")
+            add_book(title,author)
+            
+        elif user_input == "d":
+            title = input("Input book title: ")
+            remove_book(title)
+            
+        elif user_input == "r":
+            title = input("Input book title: ")
+            read_book(title)
+           
+        elif user_input == "l":
+            list_all()
+            
+
+choice()
