899c6103ec5db0510254caafc0b230274df8a890
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-22 19:36:35 +0100

    modified database.py

---
+++ Milestone_2_Datatxt/utils/database.py
@@ -0,0 +1,21 @@
+import json
+
+file = open("files_in_python/Milestone_2_Datatxt/utils/data.txt","r")
+file_content = json.load(file)
+file.close()
+
+
+def add_book():
+    pass
+
+def read_book():
+    pass
+
+def remove_book():
+    pass
+
+def list_all():
+    pass
+
+def json_func():
+    pass
