13cbace2704a08345b2899b4b88427a88d06cd56
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-22 19:36:18 +0100

    modified json_python.py

--- read_write_files/json_python.py
+++ read_write_files/json_python.py
@@ -1,5 +1,5 @@
 import json
-file = open("files_in_python/json_data.txt","r")
+file = open("read_write_files/json_data.txt","r")
 file_content = json.load(file)
 file.close()
 print(file_content["friends"][1])
@@ -11,7 +11,7 @@ books = [
     {"title":"Adventures of Mubalak","author":"Soro Jung"}
 ]
 
-create_file = open("files_in_python/json_books.txt","w")
+create_file = open("read_write_files/json_books.txt","w")
 json.dump(books,create_file)
 create_file.close()
 
