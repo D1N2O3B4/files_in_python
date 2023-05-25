39e91abb24bfa9be1873159a51f9ebd7b5eae745
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-25 23:45:00 +0100

    declared parameters in functions

---
+++ Milestone_2_Datatxt/app.py
@@ -0,0 +1,12 @@
+from utils import database
+
+
+USER_CHOICE = """Enter:
+-a to add books to the database
+-d to remove books from the database
+-r to mark book as read
+-l to list all books in the database
+-f in repairs
+-q to quit app
+"""
+
