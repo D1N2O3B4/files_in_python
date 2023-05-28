04c31b0e9abdbfef4605d8c6aec9a433d7fb18a0
D1N2O3B4 <davebostons2017@gmail.com>
2023-05-28 17:55:15 +0100

    modified database.py

--- Milestone_2_Datatxt/utils/database.py
+++ Milestone_2_Datatxt/utils/database.py
@@ -7,10 +7,21 @@ file.close()
 
 def add_book(title,author):
     with open("data.txt","a") as file:
-        file.write(f"Book Title:{title},Author{author},Read{0}")
+        file.write(f"Book Title:{title},Author{author},Read{0}\n")
 
 def read_book(title):
-    pass
+   books = list_all()
+   for book in books:
+       if book["title"] == title:
+           book["read"] = 1
+   _save_all(books)
+
+def _save_all(books):
+    with open("data.txt","w") as file:
+        for i in books:
+            file.write()
+
+
 
 def remove_book(title):
     pass
@@ -20,6 +31,9 @@ def list_all():
         all_books = [(line.strip()).split(",") for line in file.readlines()]
     
     return [{"title":book[0],"author":book[1],"read":book[2]} for book in all_books]
+
+
+
 
 def json_func():
     pass
